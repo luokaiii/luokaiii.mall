3fbe073a2fe6a403ceb63c06723014507fa04cda
luokaiii <133@163.com>
2020-10-23 17:53:22 +0800

    feat: 菜单

--- mall-admin/.umirc.ts
+++ mall-admin/.umirc.ts
@@ -5,6 +5,127 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      path: '/',
+      title: '微の商城 | 后台管理系统',
+      component: '@/layout/index',
+      routes: [
+        {
+          path: '/',
+          redirect: '/home',
+        },
+        {
+          path: '/home',
+          name: '首页',
+          component: '@/pages/home/index',
+        },
+        {
+          path: '/goods',
+          name: '商品管理',
+          routes: [
+            {
+              path: '/goods/list',
+              name: '商品管理',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/goods/category',
+              name: '分类管理',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/goods/comment',
+              name: '评价管理',
+              component: '@/pages/home/index',
+            },
+          ],
+        },
+        {
+          path: '/orders',
+          name: '订单管理',
+          routes: [
+            {
+              path: '/orders/list',
+              name: '全部订单',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/orders/WAITING',
+              name: '待发货订单',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/orders/PICK',
+              name: '待取件订单',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/orders/eval',
+              name: '待评价订单',
+              component: '@/pages/home/index',
+            },
+          ],
+        },
+        {
+          path: '/repair',
+          name: '售后管理',
+          routes: [
+            {
+              path: '/repair/WAITING',
+              name: '待处理申请',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/repair/FINISHED',
+              name: '历史售后',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/repair/REJECTED',
+              name: '已拒绝处理',
+              component: '@/pages/home/index',
+            },
+          ],
+        },
+        {
+          path: '/users',
+          name: '用户管理',
+          routes: [
+            {
+              path: '/users/list',
+              name: '用户列表',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/users/disabled',
+              name: '冻结账户',
+              component: '@/pages/home/index',
+            },
+          ],
+        },
+        {
+          path: '/system',
+          name: '后台管理',
+          component: '@/pages/home/index',
+          routes: [
+            {
+              path: '/system/users',
+              name: '账户管理',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/system/roles',
+              name: '权限管理',
+              component: '@/pages/home/index',
+            },
+            {
+              path: '/system/menus',
+              name: '菜单管理',
+              component: '@/pages/home/index',
+            },
+          ],
+        },
+      ],
+    },
   ],
 });
---
+++ mall-admin/src/layout/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ProLayout from '@ant-design/pro-layout';
+import { Link } from 'umi';
+
+import './index.less';
+
+const menuDataRender = menuList =>
+  menuList.map(item => {
+    return {
+      ...item,
+      children: item.children ? menuDataRender(item.children) : [],
+    };
+  });
+
+export default props => {
+  const { children } = props;
+  return (
+    <ProLayout
+      title="微の商城-后台"
+      menuItemRender={(menuItemProps, defaultDom) => {
+        if (menuItemProps.isUrl) {
+          return defaultDom;
+        }
+        return <Link to={menuItemProps.path}>{defaultDom}</Link>;
+      }}
+      breadcrumbRender={(routes = []) => [...routes]}
+      itemRender={(route, params, routes, paths) => {
+        const first = routes.indexOf(route) === 0;
+        return first ? (
+          <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+        ) : (
+          <span>{route.breadcrumbName}</span>
+        );
+      }}
+      rightContentRender={() => (
+        <div style={{ marginRight: '24px' }}>欢迎光临，大猪蹄子！</div>
+      )}
+      menuDataRender={menuDataRender}
+      {...props}
+    >
+      {children}
+    </ProLayout>
+  );
+};
