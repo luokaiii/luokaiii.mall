6de91be5842ea4fe718305c1c7ae8b0301d95db3
luokaiii <133@163.com>
2020-11-03 16:43:47 +0800

    feat: 抽离Header与Footer

--- mall-web/src/pages/index/index.jsx
+++ mall-web/src/pages/index/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'umi';
 
-import Headers from './components/Headers';
+import Headers from '../components/Headers';
 import Category from './components/Category';
 import SubCategory from './components/Category_sub';
 import Main from './components/Main';
-import Footer from './components/Footer';
+import Footer from '../components/Footer';
 
 import 'antd/dist/antd.css';
 import './index.less';
