94a9b7ad2edd2c50c465238bc3cb798953c36950
luokaiii <133@163.com>
2020-11-06 17:49:22 +0800

    feat: 登录页

--- mall-web/src/pages/login/index.jsx
+++ mall-web/src/pages/login/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Link } from 'umi';
 import axios from 'axios';
-import { message } from 'antd';
-import { WechatOutlined, GithubOutlined } from '@ant-design/icons';
+import { Form, message, Input, Button, Divider } from 'antd';
+import {
+  WechatOutlined,
+  GithubOutlined,
+  QqOutlined,
+  AlibabaOutlined,
+} from '@ant-design/icons';
 
+import Headers from '../components/Headers';
+import Footer from '../components/Footer';
 import './index.less';
 
 export default () => {
@@ -16,21 +22,42 @@ export default () => {
   };
 
   return (
-    <div style={{ height: '100vh' }}>
-      <div className="logo-view">
-        <Link to="/">
-          <img src={require('@/static/logo.png')} alt="logo" />
-        </Link>
-      </div>
-      <div className="openid_auth">
-        <h1>第三方登录</h1>
-        <div>
-          <WechatOutlined
-            onClick={() => message.error('暂未开放，请使用 Github 登录')}
-          />
-          <GithubOutlined onClick={() => getOAuth2Render('github')} />
+    <div>
+      <Headers />
+      <div className="login-page">
+        <div className="content">
+          <div className="login-page-left"></div>
+          <div className="login-page_right">
+            <div className="login-page_right_title">账号登录</div>
+            <Form>
+              <Form.Item name="username">
+                <Input
+                  className="login-input"
+                  placeholder="邮箱/手机号码/账户ID"
+                />
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password className="login-input" placeholder="密码" />
+              </Form.Item>
+              <Button className="login-btn" block>
+                登录
+              </Button>
+            </Form>
+            <div className="login-page_right_register">
+              <a>立即注册</a>
+              <Divider type="vertical" />
+              <a>忘记密码？</a>
+            </div>
+            <div className="login-page_right_openid">
+              <GithubOutlined />
+              <WechatOutlined />
+              <QqOutlined />
+              <AlibabaOutlined />
+            </div>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
