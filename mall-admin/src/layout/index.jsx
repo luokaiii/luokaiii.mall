b61e81e09d173fae23686f1ec13c4e1f0ba45b42
qiang.luo <qiang.luo@zznode.com>
2021-11-09 10:20:38 +0800

    feat: 升级pro-layout，支持watermark

--- mall-admin/src/layout/index.jsx
+++ mall-admin/src/layout/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ProLayout, { SettingDrawer } from '@ant-design/pro-layout';
+import ProLayout, { WaterMark } from '@ant-design/pro-layout';
 import { Link } from 'umi';
 
 import './index.less';
@@ -16,40 +16,33 @@ export default props => {
   const { children } = props;
   const [settings, setSettings] = useState({ fixSiderbar: true });
 
+  console.log('props', props);
   return (
-    <div id="mall-pro-layout" style={{ height: '100vh' }}>
-      <ProLayout
-        {...props}
-        title="微の商城-后台"
-        menuItemRender={(menuItemProps, defaultDom) => {
-          if (menuItemProps.isUrl) {
-            return defaultDom;
-          }
-          return <Link to={menuItemProps.path}>{defaultDom}</Link>;
-        }}
-        breadcrumbRender={(routes = []) => [...routes]}
-        itemRender={(route, params, routes, paths) => {
-          const first = routes.indexOf(route) === 0;
-          return first ? (
-            <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
-          ) : (
-            <span>{route.breadcrumbName}</span>
-          );
-        }}
-        rightContentRender={() => (
-          <div style={{ marginRight: '24px' }}>欢迎光临，大猪蹄子！</div>
-        )}
-        menuDataRender={menuDataRender}
-        {...settings}
-      >
-        {children}
-      </ProLayout>
-      <SettingDrawer
-        getContainer={() => document.getElementById('mall-pro-layout')}
-        settings={settings}
-        onSettingChange={val => setSettings(val)}
-        disableUrlParams
-      />
-    </div>
+    <ProLayout
+      title="微の商城-后台"
+      menuItemRender={(menuItemProps, defaultDom) => {
+        if (menuItemProps.isUrl) {
+          return defaultDom;
+        }
+        return <Link to={menuItemProps.path}>{defaultDom}</Link>;
+      }}
+      breadcrumbRender={(routes = []) => [...routes]}
+      itemRender={(route, params, routes, paths) => {
+        const first = routes.indexOf(route) === 0;
+        return first ? (
+          <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+        ) : (
+          <span>{route.breadcrumbName}</span>
+        );
+      }}
+      rightContentRender={() => (
+        <div style={{ marginRight: '24px' }}>欢迎光临，大猪蹄子！</div>
+      )}
+      menuDataRender={menuDataRender}
+      {...props}
+      {...settings}
+    >
+      <WaterMark content="微の商城">{children}</WaterMark>
+    </ProLayout>
   );
 };
--- mall-admin/src/pages/home/index.jsx
+++ mall-admin/src/pages/home/index.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Form, Input } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 export default () => {
   return (
     <PageHeaderWrapper
       title="这里是标题"
-      content={
-        <Form>
-          <Form.Item label="商品名称">
-            <Input placeholder="请输入商品名称" />
-          </Form.Item>
-        </Form>
-      }
+      content="111111"
       extraContent="额外内容"
     >
       hello world!
